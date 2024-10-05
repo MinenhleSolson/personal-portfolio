df2b06919b7ed8e0995bf934c31e68c260526e23
MinenhleSolson <minenhlecele34@gmail.com>
2024-10-05 16:51:36 +0200

    first commit

---
+++ data/index.ts
@@ -0,0 +1,196 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title:
+      "Delivering high-quality, user-friendly applications with efficient communication skills.",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with Time Zone Communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My Tech Stack",
+    description: "I Constantly Try To Improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech Enthusiast Driven by a Passion for Development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently Developing a Product Showcase Website.",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do You Want To Start a Project Together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Product Showcasing Website",
+    des: "A website that showcase available products for sale.",
+    img: "/something_spicy.svg",
+    iconLists: ["/tail.svg", "/next.svg", "/ts.svg", "/firebase.svg"],
+    link: "https://somethingspicy.co.za/",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brascswscnd, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "MongoDB",
+    img: "/mongodb.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/firebase.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "TypeScript",
+    img: "/ts.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 11,
+    name: "javascript.",
+    img: "/javascript.svg",
+    nameImg: "/dockerName.svg",
+  },
+  {
+    id: 4,
+    name: "graphql",
+    img: "/graphql.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "react",
+    img: "/react.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 6,
+    name: "php",
+    img: "/php.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 7,
+    name: "angular",
+    img: "/angular.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 8,
+    name: "node",
+    img: "/node.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 9,
+    name: "mysql",
+    img: "/mysql.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 10,
+    name: "laravel.",
+    img: "/laravel.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Full-Stack Engineer",
+    desc: "I built a fully functional auction marketplace platform with real-time rendering, from concept to deployment.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+
+  {
+    id: 3,
+    title: "Freelance App Dev Project",
+    desc: "I independently led the development of a website for a client, completing the project in less than a week and earning a perfect 5-star rating for the work",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/MinenhleSolson",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+    link: "https://x.com/minenhle_cele_",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    link: "https://www.linkedin.com/in/minenhle-solson-cele/",
+  },
+];
