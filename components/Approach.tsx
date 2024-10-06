352f34aa6f35aa433049824c85f47bc87396fe8c
MinenhleSolson <minenhlecele34@gmail.com>
2024-10-06 10:50:27 +0200

    2nd commit

--- components/Approach.tsx
+++ components/Approach.tsx
@@ -51,7 +51,7 @@ const Approach = () => {
           title="Development & Launch"
           icon={<AceternityIcon order="Phase 3" />}
           des="This is where the magic happens! Based on the approved design, 
-          I'll translate everything into functional code, building your website or Application
+          I'll translate everything into functional code, building your Website or Application
           from the ground up."
         >
           <CanvasRevealEffect
